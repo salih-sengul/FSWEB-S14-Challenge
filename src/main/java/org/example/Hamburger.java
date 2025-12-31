73c1dbbbaed4c25afa69b1e8974c38317e1612db
salih-sengul <salih.sengul@outlook.com>
2025-12-31 10:52:06 +0300

    some upsates

--- src/main/java/org/example/Hamburger.java
+++ src/main/java/org/example/Hamburger.java
@@ -1,8 +1,24 @@
 package org.example;
 
+import java.util.Objects;
+
 public class Hamburger {
     private String name;
-    meat;
+
     private int price;
     private String breadRollType;
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Hamburger hamburger = (Hamburger) o;
+        return price == hamburger.price && Objects.equals(name, hamburger.name) && Objects.equals(breadRollType, hamburger.breadRollType);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, price, breadRollType);
+    }
+
+
 }
